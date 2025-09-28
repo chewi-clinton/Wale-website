81f463cedf03c07eebbb86c295a4cded58238a0c
chewi_clinton <chewiclinton.shu@ictuniversity.edu.cm>
2025-09-28 22:27:14 +0100

    Add Dockerfile, docker-compose, and .gitignore; update requirements and settings

--- pharmacy_backend/pharmacy_backend/settings.py
+++ pharmacy_backend/pharmacy_backend/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'pharmacy',
 ]
-
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -72,7 +72,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='resend')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='dr@trimaxapharmacy.com')
-ADMIN_EMAIL = config('ADMIN_EMAIL', default='datboyken@icloud.com')
+ADMIN_EMAIL = config('ADMIN_EMAIL', default='trimaxaa@gmail.com')
 RESEND_API_KEY = config('RESEND_API_KEY', default='')
 
 
