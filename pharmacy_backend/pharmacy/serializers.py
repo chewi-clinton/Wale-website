902f072bf25e3ee7a47cf77c048c98e82ed4d804
chewi-clinton <suhchenwi48@gmail.com>
2026-02-17 11:49:57 +0100

    Add read-only fields to serializers

--- pharmacy_backend/pharmacy/serializers.py
+++ pharmacy_backend/pharmacy/serializers.py
@@ -10,11 +10,12 @@ class ProductVariantSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductVariant
         fields = '__all__'
+        read_only_fields = ('product',)
 
 class ProductSerializer(serializers.ModelSerializer):
     variants = ProductVariantSerializer(many=True, read_only=True)
     category_name = serializers.CharField(source='category.name', read_only=True)
-    
+   
     class Meta:
         model = Product
         fields = '__all__'
@@ -22,14 +23,14 @@ class ProductSerializer(serializers.ModelSerializer):
 class OrderItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name', read_only=True)
     variant_name = serializers.CharField(source='variant.name', read_only=True)
-    
+   
     class Meta:
         model = OrderItem
         fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
-    
+   
     class Meta:
         model = Order
         fields = '__all__'
